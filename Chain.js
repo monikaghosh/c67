30f68620763ba1f8d15ec922595c201045fd8280
monikaghosh <60311154+monikaghosh@users.noreply.github.com>
2021-01-18 23:09:52 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,29 @@
+class Chain{
+    constructor(bodyA, point2){
+        var options = {
+            bodyA: bodyA,
+            pointB: point2,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+
+    fly(){
+        this.chain.bodyA=null;
+    }
+
+    display(){
+        if(this.chain.bodyA){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.chain.pointB;
+        push ();
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        pop ();
+         }
+    }   
+    
+}
